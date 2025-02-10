f7d4847a3aea10332d361a4cb5c94de6d495bd5e
TzashiNorpu <tzashinorpu@gmail.com>
2025-02-10 14:22:19 +0800

    commit

--- vue3-bpmn/declarations.d.ts
+++
@@ -1,3 +0,0 @@
-// declarations.d.ts
-declare module 'bpmn-js-properties-panel'
-declare module 'bpmn-js-properties-panel/lib/provider/camunda'
--- vue3-bpmn/src/views/AboutView.vue
+++ vue3-bpmn/src/views/AboutView.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
 import BpmnModeler from 'bpmn-js/lib/Modeler'
 // 这里引入的是右侧属性栏这个框
-import { BpmnPropertiesPanelModule, BpmnPropertiesProviderModule } from 'bpmn-js-properties-panel'
+import {BpmnPropertiesPanelModule, BpmnPropertiesProviderModule, ZeebePropertiesProviderModule} from 'bpmn-js-properties-panel'
 // 而这个引入的是右侧属性栏里的内容
 // import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda'
 // 一个描述的json
-import camundaModdle from 'camunda-bpmn-moddle/resources/camunda.json'
-import { xmlStr } from '@/mock/xmlStr'
-import { onMounted, useTemplateRef, ref, unref } from 'vue'
+import ZeebeBpmnModdle from 'zeebe-bpmn-moddle/resources/zeebe.json'
+import {xmlStr} from '@/mock/xmlStr'
+import customTranslate from '@/custom-translate/custom-translate';
+const customTranslateModule = {
+  translate: ['value', customTranslate]
+};
 
+import {onMounted, useTemplateRef, ref, unref} from 'vue'
 const bpmnModeler = ref<BpmnModeler>()
 const canvas = useTemplateRef<HTMLElement>('canvas')
 async function createNewDiagram() {
   // 将字符串转换成图显示出来
-  bpmnModeler.value?.importXML(xmlStr)
+  await bpmnModeler.value?.importXML(xmlStr).then(function () {
+    console.log('Success!');
+  }).catch(function (err) {
+    console.error('Error', err);
+  });
 }
 function init() {
   bpmnModeler.value = new BpmnModeler({
@@ -21,10 +29,11 @@ function init() {
     propertiesPanel: {
       parent: '#js-properties-panel',
     },
-    additionalModules: [BpmnPropertiesPanelModule, BpmnPropertiesProviderModule],
+    additionalModules: [BpmnPropertiesPanelModule, BpmnPropertiesProviderModule, ZeebePropertiesProviderModule, customTranslateModule],
     moddleExtensions: {
       //如果要在属性面板中维护camunda：XXX属性，则需要此
-      camunda: camundaModdle,
+      // camunda: CamundaExtensionModule,
+      zeebe: ZeebeBpmnModdle
     },
   })
   createNewDiagram()
@@ -48,10 +57,12 @@ onMounted(async () => {
   width: 100%;
   height: calc(100vh - 52px);
 }
+
 .canvas {
   width: 100%;
   height: 100%;
 }
+
 .panel {
   position: absolute;
   right: 0;
